ea7102a01953d3e33256513816f1111f58e5ad27
tomsawyer0224 <130035084+tomsawyer0224@users.noreply.github.com>
2024-11-01 04:13:23 +0700

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,4 @@
+git pull
 git add .
 git commit -m "upload project"
-git remote add origin https://github.com/tomsawyer0224/handwriten-digit-recognition.git
-git pull origin main
 git push -u origin main
