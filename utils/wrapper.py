5c2ee537e0a822087cb9ad6bcd9d344e4f2c4147
tomsawyer0224 <binha0224@gmail.com>
2024-11-01 06:01:15 +0700

    update wrapper.py

--- utils/wrapper.py
+++ utils/wrapper.py
@@ -1,8 +1,25 @@
 import mlflow
+import numpy as np
+import pandas as pd
+from typing import Union
+from get_model import get_model
 
-class Wrapper(mlflow.pyfunc.PythonModel):
-	def __init__(self, config):
-		pass
+class Classifier(mlflow.pyfunc.PythonModel):
+	def __init__(self, model_config, preprocessor):
+		self.model = get_model(model_config)
+		self.preprocessor = preprocessor
+	def fit(
+			self,
+			data: Union[np.ndarray, pd.DataFrame],
+			target: Union[np.ndarray, pd.DataFrame]
+		) -> None:
+		self.model.fit(data, target)
+	def score(
+			self,
+			data: Union[np.ndarray, pd.DataFrame],
+			target: Union[np.ndarray, pd.DataFrame]
+		) -> None:
+		return self.model.score(data, target)
 	def load_context(self, context):
 		pass
 	def predict(self, context, model_input, params=None):
