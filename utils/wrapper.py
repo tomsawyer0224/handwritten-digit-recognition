ee624c0f690978bc7c1b5a622af10c629d327d5f
tomsawyer0224 <130035084+tomsawyer0224@users.noreply.github.com>
2024-11-01 05:12:03 +0700

    Update wrapper.py

--- utils/wrapper.py
+++ utils/wrapper.py
@@ -1,1 +1,10 @@
 import mlflow
+
+class Wrapper(mlflow.pyfunc.PythonModel):
+	def __init__(self):
+		pass
+	def load_context(self, context):
+		pass
+	def predict(self, context, model_input, params=None):
+		pass
+		
