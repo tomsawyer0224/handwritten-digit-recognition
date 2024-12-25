9460c95a8bce5d73e29081af9cf9bb9dd4fdf24d
tomsawyer0224 <binha0224@gmail.com>
2024-12-25 17:02:06 +0700

    modify

--- test7.py
+++ test7.py
@@ -1,20 +1,9 @@
-import mlflow
-import json
-import yaml
+from mlflow.models import convert_input_example_to_serving_input
+import numpy as np
+import pandas as pd
 
-runs = mlflow.search_runs(
-    experiment_ids=["614027524919096081"],
-    filter_string=f'tags."candidate" = "good"',
-    output_format="list",
-    order_by=["metrics.accuracy DESC"]
-)
-#print(runs)
-for run in runs:
-    print(run.info.run_name)
-    print(run.data.metrics["accuracy"])
-    print(run.data.params["model_config"])
-    print(type(run.data.params["model_config"]))
-    model_config = yaml.safe_load(run.data.params["model_config"])
-    print(model_config)
-    print(type(model_config))
-    print("-"*30)
+x = np.random.rand(2,3)
+y = pd.DataFrame(x)
+serving_input = convert_input_example_to_serving_input(y)
+print(type(serving_input))
+print(serving_input)
