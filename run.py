5b13c084b5cbbcdbbc008e104c1efa66094267d3
tomsawyer0224 <binha0224@gmail.com>
2025-01-11 06:16:14 +0700

    modify

--- run.py
+++ run.py
@@ -2,11 +2,20 @@ import argparse
 import logging
 import mlflow
 from mlflow import MlflowClient
+import click
+import yaml
 
-from core import (
-    Trainer,
-    Tuner,
-    Digit_Data_Module,
-    Toy_Data_Module
-)
+@click.group()
+def cli():
+    pass
 
+#@cli.command()
+@click.command()
+@click.argument("a", type=click.FLOAT)
+@click.argument("b", type=click.FLOAT)
+def add(a, b):
+    click.echo(a + b)
+
+cli.add_command(add)
+if __name__ == "__main__":
+    cli()
