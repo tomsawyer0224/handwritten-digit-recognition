fc338805c64584f9279b8bfc0b2c303700a9b99a
tomsawyer0224 <binha0224@gmail.com>
2025-01-11 16:05:39 +0700

    modify

--- run.py
+++ run.py
@@ -12,15 +12,27 @@ def run():
 @click.option("--config_file", type=click.File("r"))
 def init(config_file):
     os.makedirs("./scripts", exist_ok=True)
+    # script to create virtual environment
+    venv_cmds = [
+        "virtualenv .venv",
+        "source .venv/bin/activate",
+        "pip install -U pip",
+        "pip install -r requirements.txt"
+    ]
+    with open("./scripts/create_virtual_environment.sh", "w") as venv_scr:
+        venv_scr.write("\n".join(venv_cmds))
 
-    # create
+    # script to start tracking server
     config = yaml.safe_load(config_file)
     tracking_uri = urlparse(config["mlflow"]["tracking_uri"])
     host_name = tracking_uri.hostname
     port = tracking_uri.port
-    print(f"{host_name=}")
-    print(f"{port=}")
-
+    server_cmds = [
+        "source .venv/bin/activate",
+        f"mlflow server --host {host_name} --port {port}"
+    ]
+    with open("./scripts/start_tracking_server.sh", "w") as ts_scr:
+        ts_scr.write("\n".join(server_cmds))
 
 run.add_command(init)
 if __name__ == "__main__":
---
+++ scripts/start_tracking_server.sh
@@ -0,0 +1,2 @@
+source .venv/bin/activate
+mlflow server --host 127.0.0.1 --port 8000
