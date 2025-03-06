849c85461c6dff0c6087cfc7ab519b1521345be6
tomsawyer0224 <binha0224@gmail.com>
2025-03-06 15:57:20 +0700

    modify code

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,15 @@
-import mlflow
-from model.code.core import Digit_Data_Module
+import chainlit as cl
+from PIL import Image, ImageOps
 import numpy as np
 
-data_module = Digit_Data_Module()
-test_dataset = data_module.test_dataset
-
-loaded_model = mlflow.pyfunc.load_pyfunc(model_uri="model")
-test_preds = loaded_model.predict(test_dataset["data"][:10])
-print(f"predictions  = {test_preds}")
+@cl.on_message
+async def main(message: cl.Message):
+    images = [file for file in message.elements if "image" in file.mime]
+    image_paths = [image.path for image in images]
+    gray_images = [ImageOps.grayscale(Image.open(im_path)) for im_path in image_paths]
+    resized_images = [gr_im.resize((28,28)) for gr_im in gray_images]
+    normalized_images = [np.array(rs_im)/255.0 for rs_im in resized_images]
+    await cl.Message(
+        content=f"Predict: your image {np.array2string(normalized_images[0])}",
+        #elements = images
+    ).send()
